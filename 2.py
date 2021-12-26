b680c7609b4eb913a5de63020002b9ccecc9834c
ronicm <ronicohenm@gmail.com>
2021-12-26 18:56:07 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+my_number = "4"
+result = int(my_number) - 2  #float for 2.0
+print(result)
+print("your result is " + str(result) )
+print (f"your result is {result -1}")
+my_list = [10 , 30 ,60 , 80]
+# print(my_list[0:2]) # print from order 1 to 2 (not included)
+# print(my_list[0::2]) # jump in 2 and print
+print(my_list[4:0:-1])
+print(my_list[::2]) #jump in order of 2
+# # \ is used to define that the " is not
+# my_dict = {"fname" : "Roni" , "lname" : "Cohen", "age" : 45 , "id" :123234244 }
+# print  (my_dict.keys())
+# key_to_print = input(f"enter key {list(my_dict.keys())}: ")
+# print("you chose to print : " + str(my_dict[key_to_print]))
+#
