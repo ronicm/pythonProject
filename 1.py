2b490111cd4f746cb44668d6ca7dbe08172b0f6a
ronicm <ronicohenm@gmail.com>
2021-12-26 18:50:52 +0200

    add 1.py

---
+++ 1.py
@@ -0,0 +1,24 @@
+print("hello 123")
+my_var = 10
+print(my_var)
+my_name = "Roni"
+print(my_name)
+is_true = False
+my_list = ["Roni" , "cerfdefr" , 45 , True, my_name]
+print (my_list[1]) #print base on order ,starting with 0
+my_dict = dict(name="Roni", lname="Cohen", age=45)
+print(my_dict["name"])
+print(my_dict.keys())
+my_number = 5 * 2
+my_other = 5 * "Roni"
+print(my_other)
+
+if my_number == 10:
+    print("my_number")
+
+fname = "Roni"
+lname = "Cohen_M"
+
+print ("hello_2" + fname + " " + lname)
+print (f"hello {fname} {lname}")
+print ("hellodddddd %s %s" % (fname , lname ))
